100a8192fbca3e60de0efd27cd0aca1f41b161c3
sinjin25 <34289578+sinjin25@users.noreply.github.com>
2022-07-21 14:36:29 -0700

    Add SASS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Sample from './components/sample'
-import InputConvert from '@/components/input-converter/InputConvert'
+import InputConvert from './components/input-converter/InputConvert'
+import Word from './components/word/Word'
+import Kanji from './components/kanji/Kanji'
+import KanjiModel from './model/kanji-model'
+import { useState, useEffect } from 'react';
 
 function App() {
+  function createKanji() {
+    const sample = new KanjiModel('好')
+    sample.addReading('コウ')
+    sample.addDefinitions('like', 'fond of')
+    return sample.consume()
+  }
+
+  const [kData, setKData] = useState(null)
+
+  useEffect(() => {
+    setKData(createKanji())
+  }, [])
+
+  const buildKanji = () => {
+    if (!kData) return
+    return (<Kanji
+      kanji={kData.label}
+      defs={kData.data.readings}
+      readings={kData.data.definitions}
+    />)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Sample />
-        <InputConvert />
-      </header>
+      <Word str="好き"/>
+      <div>
+        {buildKanji()}
+      </div>
     </div>
   );
 }
