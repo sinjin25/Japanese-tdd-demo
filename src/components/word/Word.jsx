380396c7e24ca7382eab75dbfb47774369267a32
sinjin25 <34289578+sinjin25@users.noreply.github.com>
2022-07-24 05:11:28 -0700

    component

--- src/components/word/Word.jsx
+++ src/components/word/Word.jsx
@@ -1,8 +1,41 @@
-export default function({label, data}) {
+import { isKanji } from 'wanakana'
+import Kanji from '../single/kanji/Kanji'
+export default function({label, data, kanjiInfo}) {
     
+    // TODO: Convert label to kanji info
+    const displayWord = () => {
+        const results = []
+        for (let i = 0; i < label.length; i++) {
+            const char = label[i]
+            console.log(char)
+            results.push(convertLabelToComponents(char))
+        }
+        console.log(results)
+        return (<div>
+            {
+                results.map((i, index) => {
+                    return <span key={index}>{i}</span>
+                })
+            }
+        </div>)
+    }
+    const convertLabelToComponents = (char) => {
+        const found = kanjiInfo.find(i => i.label === char)
+        if (isKanji(char) && found) {
+            console.log('!!!', found)
+            return (
+                <Kanji
+                kanji={found.label}
+                defs={found.data.definitions}
+                readings={found.data.readings}
+                />
+            )
+        }
+        return char
+    }
     return (
         <div>
-            <div>123S</div>
+            {displayWord()}
             <h2>{label}</h2>
         </div>
     )
