b7e8c1e46c53f59a7488b033abeed27711b9678e
sinjin25 <34289578+sinjin25@users.noreply.github.com>
2022-07-26 10:58:52 -0700

    Allow parent to control aspects of InputConvert component

--- src/components/single/input-converter/InputConvert.jsx
+++ src/components/single/input-converter/InputConvert.jsx
@@ -1,19 +1,54 @@
 import converter from '../../../js/jp-input'
 import { useState } from 'react'
 
-export default ({placeholderText}) => {
+export default ({
+    placeholderText,
+    convertToJp = false,
+    typable = true,
+    hSubmit
+}) => {
 
     let [value, setValue] = useState('')
 
     const handleChange = (e) => {
         const {target} = e
         const v = target.value
-        setValue(converter(v, false))
+        if (convertToJp) setValue(converter(v, false))
+        else setValue(v)
     }
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault()
+            if (hSubmit) {
+                const valueToSend = finalize()
+                console.log('handlekey', valueToSend)
+                hSubmit(valueToSend)
+            }
+            return
+        }
+    }
+
+    const handleKeyDown = (e) => {
+        if (typable === false) {
+            e.preventDefault()
+            return
+        }
+    }
+
+    const finalize = () => {
+        const finalVal = value
+        setValue('')
+        console.log('finalizing val', finalVal)
+        return finalVal
+    }
+    
     return (
         <input
         value={value}
         placeholder={placeholderText || 'Type a word'}
+        onKeyDown={handleKeyDown}
+        onKeyUp={handleKeyPress}
         onChange={handleChange} />
     )
 }
