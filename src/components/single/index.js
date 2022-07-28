d6f92ad8dada42d65470c93c4b9d389022a01141
sinjin25 <34289578+sinjin25@users.noreply.github.com>
2022-07-28 01:46:41 -0700

    Move single components exports into an index file

---
+++ src/components/single/index.js
@@ -0,0 +1,3 @@
+export { default as Definition } from './definition/Definition'
+export { default as InputConverter } from './input-converter/InputConvert'
+export { default as Kanji } from './kanji/Kanji'
